eb4230926a06f1973a24393e1e5f0b518edc6008
kotesh79 <vrkeats@yahoo.com>
2016-04-28 16:45:39 +0530

    Added files via upload

--- app/mock/mock-service-urls.js
+++ app/mock/mock-service-urls.js
@@ -1,1 +1,24 @@
+'use strict';
 
+angular
+    .module('bidAuctionApp', ['ngMockE2E'])
+    .run(function($httpBackend, $resource, configuration) {
+
+        // Common pass through urls
+        $httpBackend.whenGET(/assets\/languages\/.*/).passThrough();
+        $httpBackend.whenGET(/\.html.*$/).passThrough();
+        $httpBackend.whenGET(/^api\/.*/).passThrough();
+
+        // TODO: This should be mocked!
+        $httpBackend.whenPOST(/\/ng_logout/).passThrough();
+
+        // Service URLs
+        mock(false, $httpBackend.whenGET(/\/product\/.*/)).respond(
+            $resource('api/productManagement/product.json').get());
+
+        mock(false, $httpBackend.whenGET(/\/rest\/userManagement\/getPlayerDetails.*/))
+            .respond($resource('api/userManagement/playerDetails.json').get());
+
+        mock(false, $httpBackend.whenGET(/\/rest\/userManagement\/getSellerDetails.*/))
+            .respond($resource('api/userManagement/sellerDetails.json').get());
+    });
