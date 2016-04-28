0abd1afd5410c9b4ec809708188a81758648e80b
kotesh79 <vrkeats@yahoo.com>
2016-04-28 21:44:14 +0530

    Added files via upload

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -1,22 +1,22 @@
 'use strict';
 
-describe('Controller: AboutCtrl', function () {
+describe('Controller: MainCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('bidAuctionApp'));
+  //beforeEach(module('bidAuctionApp'));
 
-  var AboutCtrl,
+  /*var MainCtrl,
     scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    AboutCtrl = $controller('AboutCtrl', {
+    MainCtrl = $controller('DashboardCtrl', {
       $scope: scope
     });
   }));
 
   it('should attach a list of awesomeThings to the scope', function () {
     expect(scope.awesomeThings.length).toBe(3);
-  });
+  });*/
 });
