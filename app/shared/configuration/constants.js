d177c862b7bb14501b02ba2fb7598967d9db1b5e
kotesh79 <vrkeats@yahoo.com>
2016-04-28 21:30:12 +0530

    Added files via upload

--- app/shared/configuration/constants.js
+++ app/shared/configuration/constants.js
@@ -1,1 +1,7 @@
-
+(function() {
+    'use strict';
+     angular.module('bidAuctionApp').constant('kbconstants', {
+        authServiceBaseUrl: '',
+        serviceBaseUrl: 'http://localhost:9000/rest'
+     });
+})();
